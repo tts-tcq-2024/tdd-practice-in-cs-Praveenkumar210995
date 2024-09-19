e7d92795337a55af0d9c1d29735578e9fd83c6c0
Praveenkumar210995 <pravin21995@gmail.com>
2024-09-19 15:44:55 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,13 @@
+using System.Linq;
 
+public class StringCalculator
+{
+    public int Add(string input)
+    {
+        if (checkNullString(input))
+            return 0;
+    }
+
+  private static bool checkNullString(string input)
+    { return string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input); }
+}
