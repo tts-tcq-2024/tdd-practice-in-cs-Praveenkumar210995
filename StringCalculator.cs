b7827e2a5d77d79e1086d07fbd515e68dd26436e
Praveenkumar210995 <pravin21995@gmail.com>
2024-09-20 08:37:52 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -6,39 +6,11 @@ public class StringCalculator
     {
         if (checkNullString(input))
             return 0;
-        var numberlist = inputDecoder(input);
-        return numberlist;
+        
+        return 0;
     }
 
   private static bool checkNullString(string input)
     { return string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input); }
 
-  private static List<int> inputDecoder(string numbers)
-    {
-        string[] delimiters = new string[] { ",", "\n" };
-        if (numbers.StartsWith("//"))
-        {
-            int delimiterIndex = numbers.IndexOf('\n');
-            string customDelimiter = numbers.Substring(2, delimiterIndex - 2);
-            delimiters = new string[] { customDelimiter };
-            numbers = numbers.Substring(delimiterIndex + 1);
-        }
-        string[] numberStrings = numbers.Split(delimiters, StringSplitOptions.None);
-        return getNumberList(numberStrings);
-    }
-      private static List<int> getNumberList(string[] numberStrings)
-    {
-        var numberList = new List<int>();
-        foreach (var numberString in numberStrings)
-        {
-            if (int.TryParse(numberString, out int number))
-            {
-                if (number <= 1000)
-                {
-                    numberList.Add(number);
-                }
-            }
-        }
-        return numberList;
-    }
 }
